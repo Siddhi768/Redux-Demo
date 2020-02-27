1c59d62433a2ac9225ee3c9f8577cfe64f507169
Siddhi768 <siddhiyadav768@gmail.com>
2020-02-27 14:55:39 +0530

    Trial commit 2

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello
+          Hello, Welcome Siddhi!
         </p>
         <a
           className="App-link"
